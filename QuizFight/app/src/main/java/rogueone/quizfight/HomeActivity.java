da0dd3d7d96c7144ff90fb9896c6d4c96ee43176
abeccaro <alex.becks@hotmail.it>
2017-05-19 18:15:55 +0200

    Remade duel list with ListView and relative adapter

--- QuizFight/app/src/main/java/rogueone/quizfight/HomeActivity.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/HomeActivity.java
@@ -1,11 +1,20 @@
 package rogueone.quizfight;
 
-import android.content.res.Resources;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,37 +29,20 @@ public class HomeActivity extends AppCompatActivity {
         int[][] scores = {{10,7},{5,9},{10,8},{9,9},{12,8}};
 
         // setting username from login
-        TextView usernameView = (TextView) findViewById(R.id.UsernameView);
+        final TextView usernameView = (TextView) findViewById(R.id.UsernameView);
         usernameView.setText(username);
 
         // if there's at least one old duel hide empty message and show old duels list
         if (opponents.length > 0) {
             findViewById(R.id.NoDuels).setVisibility(View.GONE);
-            findViewById(R.id.LastDuels).setVisibility(View.VISIBLE);
             findViewById(R.id.DuelsHistory).setVisibility(View.VISIBLE);
+            findViewById(R.id.ListTest).setVisibility(View.VISIBLE);
         }
 
-        // for each of the five last duels set relative overview
-        for (int i = 0; i < opponents.length; i++) {
-            Resources res = getResources();
 
-            // getting views
-            int id = res.getIdentifier("LastDuel"+i+"Icon", "id", getApplicationContext().getPackageName());
-            ImageView icon = (ImageView) findViewById(id);
-            id = res.getIdentifier("LastDuel"+i+"User", "id", getApplicationContext().getPackageName());
-            TextView opponent = (TextView) findViewById(id);
-            id = res.getIdentifier("LastDuel"+i+"Score", "id", getApplicationContext().getPackageName());
-            TextView score = (TextView) findViewById(id);
+        final ListView listview = (ListView) findViewById(R.id.ListTest);
 
-            // setting win/loss/tie icon, opponent name and score
-            if (scores[i][0] > scores[i][1])
-                icon.setImageResource(R.drawable.victory);
-            else if (scores[i][0] == scores[i][1])
-                icon.setImageResource(R.drawable.tie);
-            else
-                icon.setImageResource(R.drawable.defeat);
-            opponent.setText(opponents[i]);
-            score.setText(scores[i][0]+" - "+scores[i][1]);
-        }
+        final DuelSummaryAdapter listAdapter = new DuelSummaryAdapter(this, opponents, scores);
+        listview.setAdapter(listAdapter);
     }
 }

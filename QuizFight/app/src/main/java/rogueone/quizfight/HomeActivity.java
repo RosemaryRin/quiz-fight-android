21123e1dfe9ca6c03510ce48c89d5156a4b782fd
mdipirro <matteodipirro@gmail.com>
2017-05-23 18:46:11 +0200

    First attempt to show actual data

--- QuizFight/app/src/main/java/rogueone/quizfight/HomeActivity.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/HomeActivity.java
@@ -1,6 +1,8 @@
 package rogueone.quizfight;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,31 +11,45 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.gms.games.Games;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import rogueone.quizfight.loaders.SavedGamesLoader;
+import rogueone.quizfight.models.SavedGames;
+
+public class HomeActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<SavedGames> {
 
     private static final int DUELS_SHOWN = 5;
+    private static final int SAVED_GAMES_LOADER = 1;
+
+    private SavedGames savedState;
+
+    @BindView(R.id.UsernameView) TextView username;
+    @BindView(R.id.ListTest) ListView oldDuels_listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        getLoaderManager().initLoader(SAVED_GAMES_LOADER, null, this);
 
         //FIXME: get proper data from login and db
-        String username = "abeccaro";
         String[] opponents = {"mdipirro", "emanuelec", "rajej", "Pinco Pallo", "Tizio", "Caio", "Sempronio", "Player1234", "Tua mamma", "armir"};
         int[][] scores = {{10,7},{5,9},{10,8},{9,9},{12,8},{10,7},{5,9},{10,8},{9,9},{12,8}};
 
         // setting username from login
-        final TextView usernameView = (TextView) findViewById(R.id.UsernameView);
-        usernameView.setText(username);
+        username.setText(Games.Players.getCurrentPlayer(
+                ((QuizFightApplication)getApplicationContext()).getClient()
+        ).getDisplayName());
 
         // if there's at least one old duel hide empty message and show old duels list
-        if (opponents.length > 0) {
-            final ListView listView = (ListView) findViewById(R.id.ListTest);
+        /*if (opponents.length > 0) {
 
-            findViewById(R.id.NoDuels).setVisibility(View.GONE);
-            findViewById(R.id.DuelsHistory).setVisibility(View.VISIBLE);
-            listView.setVisibility(View.VISIBLE);
+
 
             // initializing showed data arrays
             String[] opponentsShown = new String[DUELS_SHOWN];
@@ -49,7 +65,7 @@ public class HomeActivity extends AppCompatActivity {
 
             final DuelSummaryAdapter listAdapter = new DuelSummaryAdapter(this, opponentsShown, scoresShown);
             listView.setAdapter(listAdapter);
-        }
+        }*/
 
 
         // duels history button
@@ -72,4 +88,29 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public Loader<SavedGames> onCreateLoader(int id, Bundle args) {
+        return new SavedGamesLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<SavedGames> loader, SavedGames data) {
+        ((QuizFightApplication)getApplicationContext()).setSavedGames(data);
+        savedState = data;
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (savedState!= null && !savedState.isEmpty()) {
+            findViewById(R.id.NoDuels).setVisibility(View.GONE);
+            findViewById(R.id.DuelsHistory).setVisibility(View.VISIBLE);
+            oldDuels_listview.setVisibility(View.VISIBLE);
+            final DuelSummaryAdapter listAdapter = new DuelSummaryAdapter(this, savedState.getDuels());
+            oldDuels_listview.setAdapter(listAdapter);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<SavedGames> loader) {}
 }

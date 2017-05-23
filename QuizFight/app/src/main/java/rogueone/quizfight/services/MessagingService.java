f0b94be2acef71ea33d7bcc566811c391196252e
mdipirro <matteodipirro@gmail.com>
2017-05-23 15:39:19 +0200

    Add notification management

--- QuizFight/app/src/main/java/rogueone/quizfight/services/MessagingService.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/services/MessagingService.java
@@ -4,56 +4,72 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import rogueone.quizfight.HomeActivity;
+import java.util.Map;
+
+import rogueone.quizfight.R;
+
+import static rogueone.quizfight.NotificationFactory.getTargetActivity;
 
 /**
  * Created by mdipirro on 20/05/17.
  */
 
 public class MessagingService extends FirebaseMessagingService {
+    private static final String title = "title";
+    private static final String message = "message";
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        //TODO save body
-        //TODO check for different notification types? --> id from server to be used in sendNotification
-        //      TODO maybe using a factory/command for handling different actions
-        if (remoteMessage.getNotification() != null) {
-            sendNotification(
-                    remoteMessage.getNotification().getTitle(),
-                    remoteMessage.getNotification().getBody()
-            );
+        Map<String, String> body = remoteMessage.getData();
+        if (body != null) {
+            if (body.containsKey(title) && body.containsKey(message)) {
+                sendNotification(body);
+            }
         }
     }
 
     /**
      * Create and show a simple notification containing the received FCM message.
      *
-     * @param title FCM title received
-     * @param notificationBody FCM message body received.
+     * @param body FCM body received
      */
-    private void sendNotification(String title, String notificationBody) {
-        Intent intent = new Intent(this, HomeActivity.class);
+    private void sendNotification(@NonNull Map<String, String> body) {
+        int id = Integer.parseInt(body.get("id"));
+        Intent intent = new Intent(this, getTargetActivity(id));
+        populateIntent(body, intent);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, id, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setContentTitle(title)
-                .setContentText(notificationBody)
+                .setContentTitle(body.get(title))
+                .setContentText(body.get(message))
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(id, notificationBuilder.build());
+    }
+
+    private void populateIntent(@NonNull Map<String, String> body, @NonNull Intent intent) {
+        for (Map.Entry<String, String> entry : body.entrySet()) {
+            if (!entry.getKey().equals(title) && !entry.getKey().equals(message)) {
+                intent.putExtra(entry.getKey(), entry.getValue());
+            }
+        }
     }
 }

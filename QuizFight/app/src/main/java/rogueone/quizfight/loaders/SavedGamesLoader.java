5805922ab67ed74e2fbe6d60b83dcaece7bb8dfe
mdipirro <matteodipirro@gmail.com>
2017-05-24 19:38:37 +0200

    Add conflict resolution

--- QuizFight/app/src/main/java/rogueone/quizfight/loaders/SavedGamesLoader.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/loaders/SavedGamesLoader.java
@@ -2,8 +2,10 @@ package rogueone.quizfight.loaders;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.widget.Toast;
 
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesStatusCodes;
 import com.google.android.gms.games.snapshot.Snapshot;
 import com.google.android.gms.games.snapshot.Snapshots;
 
@@ -22,6 +24,8 @@ import rogueone.quizfight.models.SavedGames;
 
 public class SavedGamesLoader extends AsyncTaskLoader<SavedGames> {
 
+    private static final int MAX_SNAPSHOT_RESOLVE_RETRIES = 3;
+
     public SavedGamesLoader(Context context) {
         super(context);
     }
@@ -38,18 +42,59 @@ public class SavedGamesLoader extends AsyncTaskLoader<SavedGames> {
                 ((QuizFightApplication)getContext()).getClient(),
                 getContext().getString(R.string.snapshot_name),
                 true).await();
+        Snapshot snapshot = processSnapshotOpenResult(result, 0);
 
-        if (result.getStatus().isSuccess()) {
-            Snapshot snapshot = result.getSnapshot();
+        if (snapshot != null) {
             // Read the byte content of the saved game.
             try {
                 return byteToObject(snapshot.getSnapshotContents().readFully());
             } catch (IOException e) {
                 return null;
             }
-        } else{
+        } else {
             return null;
         }
+    }
+
+    /**
+     * Conflict resolution for when Snapshots are opened.  Must be run in an AsyncTask or in a
+     * background thread,
+     */
+    Snapshot processSnapshotOpenResult(Snapshots.OpenSnapshotResult result, int retryCount) {
+        Snapshot snapshot = null;
+        retryCount++;
+
+        int status = result.getStatus().getStatusCode();
+
+        if (status == GamesStatusCodes.STATUS_OK) {
+            return result.getSnapshot();
+        } else if (status == GamesStatusCodes.STATUS_SNAPSHOT_CONTENTS_UNAVAILABLE) {
+            return result.getSnapshot();
+        } else if (status == GamesStatusCodes.STATUS_SNAPSHOT_CONFLICT) {
+            Snapshot currentSnapshot = result.getSnapshot();
+            Snapshot conflictSnapshot = result.getConflictingSnapshot();
+
+            snapshot = (currentSnapshot.getMetadata().getLastModifiedTimestamp() <
+                    conflictSnapshot.getMetadata().getLastModifiedTimestamp()) ?
+                        conflictSnapshot :
+                        currentSnapshot;
+
+            Snapshots.OpenSnapshotResult resolveResult = Games.Snapshots.resolveConflict(
+                    ((QuizFightApplication)getContext()).getClient(),
+                    result.getConflictId(),
+                    snapshot).await();
+
+            if (retryCount < MAX_SNAPSHOT_RESOLVE_RETRIES) {
+                return processSnapshotOpenResult(resolveResult, retryCount);
+            } else {
+                // Failed, log error and show Toast to the user
+                String message = "Could not resolve snapshot conflicts";
+                Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+            }
+
+        }
+        // Fail, return null.
+        return null;
     }
 
     private SavedGames byteToObject(byte[] data) {

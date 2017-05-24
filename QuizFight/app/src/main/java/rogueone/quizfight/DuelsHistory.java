a47d6e5f393c6aac82b3d821268e8eb744d05ba6
mdipirro <matteodipirro@gmail.com>
2017-05-24 19:46:56 +0200

    Add SavedGames information

--- QuizFight/app/src/main/java/rogueone/quizfight/DuelsHistory.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/DuelsHistory.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
 
+import rogueone.quizfight.models.SavedGames;
+
 public class DuelsHistory extends AppCompatActivity {
 
     @Override
@@ -13,25 +15,17 @@ public class DuelsHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duels_history);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        //FIXME: get proper data from login and db
-        String username = "abeccaro";
-        String[] opponents = {"mdipirro", "emanuelec", "rajej", "Pinco Pallo", "Tizio", "Caio", "Sempronio", "Player1234", "Tua mamma", "armir"};
-        int[][] scores = {{10,7},{5,9},{10,8},{9,9},{12,8},{10,7},{5,9},{10,8},{9,9},{12,8}};
+        SavedGames savedState = ((QuizFightApplication)getApplicationContext()).getSavedGames();
 
         // if there's at least one old duel hide empty message and show old duels list
-        if (opponents.length > 0) {
-            //FIXME
-            /*final ListView listView = (ListView) findViewById(R.id.history);
+        if (savedState!= null && !savedState.isEmpty()) {
+            final ListView listView = (ListView) findViewById(R.id.history);
 
             findViewById(R.id.noDuels).setVisibility(View.GONE);
             listView.setVisibility(View.VISIBLE);
 
-            final DuelSummaryAdapter listAdapter = new DuelSummaryAdapter(this, opponents, scores);
-            listView.setAdapter(listAdapter);*/
+            final DuelSummaryAdapter listAdapter = new DuelSummaryAdapter(this, savedState.getDuels());
+            listView.setAdapter(listAdapter);
         }
     }
 }

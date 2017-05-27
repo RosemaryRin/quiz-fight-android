62797db44970ee238c4cd951615ea47f2719224f
abeccaro <alex.becks@hotmail.it>
2017-05-27 20:02:45 +0200

    Duel detail activity now shows global result

--- QuizFight/app/src/main/java/rogueone/quizfight/DuelDetailActivity.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/DuelDetailActivity.java
@@ -2,6 +2,10 @@ package rogueone.quizfight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import rogueone.quizfight.models.Duel;
+import rogueone.quizfight.models.Score;
 
 public class DuelDetailActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class DuelDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duel_detail);
+
+        Duel duel = (Duel) getIntent().getSerializableExtra("Duel");
+
+        // opponent name
+        TextView opponentName = (TextView) findViewById(R.id.textview_dueldetail_opponent);
+        opponentName.setText(duel.getOpponent());
+
+        // global score
+        Score score = duel.getScore();
+        TextView scoreView = (TextView) findViewById(R.id.textview_dueldetail_globalscore);
+        scoreView.setText(score.getPlayerScore() + " - " + score.getOpponentScore());
     }
 
 }

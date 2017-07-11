816f97ee279399e0290f1ea2477954491b4716e7
EmanueleC <emanuele.carraro94@gmail.com>
2017-07-11 18:50:03 +0200

    removed unused import

--- QuizFight/app/src/main/java/rogueone/quizfight/HomeActivity.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/HomeActivity.java
@@ -6,14 +6,11 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;

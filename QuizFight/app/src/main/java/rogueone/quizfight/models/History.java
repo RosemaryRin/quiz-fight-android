d63a15385a3cc46bd4e319d8a0d84bcaf3d4d1fe
Matteo Di Pirro <matteodipirro@gmail.com>
2017-07-16 15:16:22 +0200

    Return the last n duels, and not the first n

--- QuizFight/app/src/main/java/rogueone/quizfight/models/History.java
+++ QuizFight/app/src/main/java/rogueone/quizfight/models/History.java
@@ -31,15 +31,9 @@ public class History implements Serializable {
     }
 
     public List<Duel> getCompletedDuels(int n) {
-        List<Duel> inProgress = new ArrayList<>();
-        Iterator<Duel> iterator = duels.iterator();
-        while (iterator.hasNext() && inProgress.size() < n) {
-            Duel duel = iterator.next();
-            if (duel.isCompleted()) {
-                inProgress.add(duel);
-            }
-        }
-        return inProgress;
+        List<Duel> completedDuels = getCompletedDuels();
+        int size = completedDuels.size();
+        return completedDuels.subList(size - n, size);
     }
 
     public List<Duel> getInProgressDuels() {
